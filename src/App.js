be4225215ef427d3ff9e209177f3fbaeb4526acb
Hoang An Unicode <hoanganit19@gmail.com>
2022-05-08 01:34:01 +0700

    Update code 08/05/2022

--- src/App.js
+++ src/App.js
@@ -5,98 +5,54 @@ import React from 'react';
 
 function App() {
 
-  // const element = React.createElement(
-  //     'div', 
-  //     {}, 
-  //     React.createElement(
-  //       'div',
-  //       {className: 'block'},
-  //       React.createElement(
-  //         'h2',
-  //         {
-  //           className: 'sub-title',
-  //           id: 'sub-title'
-  //         },
-  //         'Khoá học ReactJS'
-  //       ),
-    
-  //       React.createElement(
-  //         'h1',
-  //         {
-  //           className: 'title',
-  //           id: 'title'
-  //         },
-  //         'Unicode Academy'
-  //       ),
-    
-  //       React.createElement(
-  //         'p',
-  //         {
-  //           className: 'text'
-  //         },
-  //         React.createElement(
-  //           'a',
-  //           {
-  //             href: 'https://unicode.vn',
-  //             target: '_blank',
-  //             className: 'text--link',
-  //             title: 'Đăng ký khoá học'
-  //           },
-  //           'Đăng ký ngay'
-  //         )
-  //       )
-  //     ),
-  //     React.createElement(
-  //       'div',
-  //       {
-  //         className: 'element'
-  //       },
-  //       'Khoá học lập trình tại Unicode'
-  //     )
-  //   );
+  /*
+  1. Render có điều kiện
+  - Dùng câu lệnh if thông thường
+  - Dùng toán tử 3 ngôi
+  2. Render với vòng lặp (map)
+  */
 
-  const firstProvince = 1;
+  const products = [
+    {
+      id: 1,
+      name: 'Sản phẩm 1',
+      price: 12000
+    },
+    {
+      id: 2,
+      name: 'Sản phẩm 2',
+      price: 13000
+    },
+    {
+      id: 3,
+      name: 'Sản phẩm 3',
+      price: 14000
+    },
 
-  const styleElement = {
-    color: 'red',
-    fontWeight: 'bold',
-    fontSize: '1.5rem'
-  };
+    {
+      id: 4,
+      name: 'Sản phẩm 4',
+      price: 15000
+    }
+  ];
 
-  const myAlert = ()=>{
-    alert('Unicode Academy');
-  }
-
-  const welcomeContent = `Khoá học lập trình tại Unicode`;
-
-  return (
-    <div>
-      <div className='block'>
-          <h2 className='sub-title' id='sub-title'>Khoá học ReactJS</h2>
-          <h1 className='title' id='title'>Unicode Academy</h1>
-          <p className='text'>
-            <a href='https://unicode.vn' className='text--link' target="_blank" title='Đăng ký khoá học'>Đăng ký ngay</a>
-          </p>
+  const productLists = products.map((item, index)=>{
+    return (
+      <div key={index}>
+        <p>ID: {item.id}</p>
+        <p>Tên: {item.name}</p>
+        <p>Giá: {item.price}</p>
+        <hr/>
       </div>
-      <div className='element' onClick={myAlert} style={styleElement}>
-        {welcomeContent}
-      </div>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username" name="username" defaultValue="hoangan.web" />
-      <div>
-        <select>
-          <option value="0">Chọn Tỉnh/Thành phố</option>
-          <option value={firstProvince}>Hà Nội</option>
-        </select>
-      </div>
+    )
+  });
 
-      <div className="awesome" style={{ border: "1px solid red" }}>
-        <label htmlFor="name">Enter your name: </label>
-        <input type="text" id="name" />
-      </div>
-      <p>Enter your HTML here</p>
-    </div>
-  );
+    return(
+      <>
+        <h2>Danh sách sản phẩm</h2>
+        {productLists}
+      </>
+    )
 }
 
 export default App;
