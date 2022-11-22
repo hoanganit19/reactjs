2888409b7d0ae7cb3596cbb9a4e6d5f9fdb59106
Hoang An <contact@unicode.vn>
2022-11-22 23:54:03 +0700

    Validate Form

--- src/components/state/Form.js
+++ src/components/state/Form.js
@@ -2,39 +2,67 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export class Form extends Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
-        form: {
-            email: '',
-            password: ''
-        }
-    }
-  }  
+      form: {
+        email: "",
+        password: "",
+      },
+      errors: {},
+    };
+  }
 
   handleChangeValue = (e) => {
-    const data = {...this.state.form};
+    const data = { ...this.state.form };
     data[e.target.name] = e.target.value;
 
     this.setState({
-        form: data
-    })
-  }
+      form: data,
+    });
+  };
 
   handleSubmitForm = (e) => {
     e.preventDefault();
-    const {email, password} = this.state.form;
-    console.log(email, password);
-  }
+    const { email, password } = this.state.form;
+
+    const errors = {}; //Lưu trữ các lỗi
+
+    if (typeof email === "string" && email.trim() === "") {
+      errors.email = "Email không được để trống";
+    }
+
+    if (typeof password === "string" && password.trim() === "") {
+      errors.password = "Mật khẩu không được để trống";
+    }
+
+    this.setState({
+      errors: errors,
+    });
+
+    if (Object.keys(errors).length == 0) {
+      this.setState({
+        form: {
+          email: "",
+          password: "",
+        },
+      });
+    }
+  };
 
   render() {
-    const {form} = this.state;
-   
+    const { form, errors } = this.state;
+    const { email, password } = form;
+
     return (
       <div className="container py-5">
         <div className="row justify-content-center">
           <div className="col-6">
+            {Object.keys(errors).length > 0 && (
+              <div className="alert alert-danger text-center">
+                Vui lòng kiểm tra lại dữ liệu
+              </div>
+            )}
             <form onSubmit={this.handleSubmitForm}>
               <div className="mb-3">
                 <label htmlFor="email">Email</label>
@@ -42,10 +70,14 @@ export class Form extends Component {
                   type="email"
                   id="email"
                   name="email"
-                  className="form-control"
+                  className={`form-control ${errors.email ? "is-invalid" : ""}`}
                   placeholder="Email..."
                   onChange={this.handleChangeValue}
+                  value={email}
                 />
+                {errors.email && (
+                  <div className="invalid-feedback">{errors.email}</div>
+                )}
               </div>
               <div className="mb-3">
                 <label htmlFor="password">Mật khẩu</label>
@@ -53,10 +85,16 @@ export class Form extends Component {
                   type="password"
                   id="password"
                   name="password"
-                  className="form-control"
+                  className={`form-control ${
+                    errors.password ? "is-invalid" : ""
+                  }`}
                   placeholder="Mật khẩu..."
                   onChange={this.handleChangeValue}
+                  value={password}
                 />
+                {errors.password && (
+                  <div className="invalid-feedback">{errors.password}</div>
+                )}
               </div>
               <button type="submit" className="btn btn-primary">
                 Đăng nhập
